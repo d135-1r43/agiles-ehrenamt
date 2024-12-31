f2dda595cb6b2db8c7afca81333cea144ad43a79
Markus Herhoffer <markus@herhoffer.net>
2024-12-31 20:39:21 +0100

    📝 (#11): Add Impressum

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,127 +5,127 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Agiles Ehrenamt',
-  tagline: 'ehrenamtlich engagiert, agil organisiert',
-  favicon: 'img/favicon.ico',
+    title: 'Agiles Ehrenamt',
+    tagline: 'ehrenamtlich engagiert, agil organisiert',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://agiles-ehrenamt.de',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://agiles-ehrenamt.de',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'de',
-    locales: ['de'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'de',
+        locales: ['de'],
+    },
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/d135-1r43/agiles-ehrenamt/tree/main',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/d135-1r43/agiles-ehrenamt/tree/main',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/d135-1r43/agiles-ehrenamt/tree/main',
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ['rss', 'atom'],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/d135-1r43/agiles-ehrenamt/tree/main',
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: 'warn',
+                    onInlineAuthors: 'warn',
+                    onUntruncatedBlogPosts: 'warn',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/social-card.png',
-    navbar: {
-      title: 'Agiles Ehrenamt',
-      logo: {
-        alt: 'Agiles Ehrenamt',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Dokumentation',
-        },
-        // {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/d135-1r43/agiles-ehrenamt/',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Dokumentation',
-              to: '/docs/intro',
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/social-card.png',
+        navbar: {
+            title: 'Agiles Ehrenamt',
+            logo: {
+                alt: 'Agiles Ehrenamt',
+                src: 'img/logo.svg',
             },
-          ],
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Dokumentation',
+                },
+                // {to: '/blog', label: 'Blog', position: 'left'},
+                {
+                    href: 'https://github.com/d135-1r43/agiles-ehrenamt/',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Open Project e.V. Pfaffenhofen',
-              href: 'https://op-paf.de',
-            },
-            {
-              label: 'CVJM Jugendkultur & Musik e.V.',
-              href: 'https://blastofeternity.de',
-            },
-          ],
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Dokumentation',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Open Project e.V. Pfaffenhofen',
+                            href: 'https://op-paf.de',
+                        },
+                        {
+                            label: 'CVJM Jugendkultur & Musik e.V.',
+                            href: 'https://blastofeternity.de',
+                        },
+                    ],
+                },
+                {
+                    title: 'Förderer',
+                    items: [
+                        {
+                            label: 'Mit freundlicher Unterstützung der Deutschen Stiftung für Engagement und Ehrenamt',
+                            to: 'https://www.deutsche-stiftung-engagement-und-ehrenamt.de/',
+                        },
+                    ],
+                },
+            ],
+            copyright: `<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.de"><img src="/img/cc-by-nc-nd.svg" alt="CC BY-NC-ND logo" style="height: 2em;" /></a>. <br/>
+                        Copyright © ${new Date().getFullYear()} &middot; <a href="/impressum">Impressum</a> &middot; erstellt mit <a href="https://docusaurus.io/">Docusaurus</a>`,
         },
-        {
-          title: 'Förderer',
-          items: [
-            {
-              label: 'Mit freundlicher Unterstützung der Deutschen Stiftung für Engagement und Ehrenamt',
-              to: 'https://www.deutsche-stiftung-engagement-und-ehrenamt.de/',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-      ],
-      copyright: `<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.de"><img src="/img/cc-by-nc-nd.svg" alt="CC BY-NC-ND logo" style="height: 2em;" /></a>. <br/>
-                        Copyright © ${new Date().getFullYear()} &middot; Built with <a href="https://docusaurus.io/">Docusaurus</a>`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
